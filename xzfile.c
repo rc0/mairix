a169d83ace8d4a19c44f81cf9782a4768f0f6f99
Conrad Hughes <debbugs@xrad.org>
2020-01-18 00:40:09 +0000

    Improved .xz file reader implementation.

--- xzfile.c
+++ xzfile.c
@@ -32,23 +32,40 @@
 
 #include "xzfile.h"
 
+/* Input buffer size. */
+#define IBSZ 65536
+
+static const lzma_stream s_lzma_init = LZMA_STREAM_INIT;
+
 struct xzFile_s {
-  FILE *m_fp;
+  FILE        *m_fp;
+  lzma_stream  m_lzma;
+  uint8_t      m_inbuf[IBSZ];
 };
 
 xzFile *
 xzopen(const char *filename, const char *mode)
 {
-  xzFile *xzf;
+  xzFile      *xzf;
+  lzma_stream *lzs;
 
   if(filename == NULL || strcmp(mode, "rb") != 0)
     goto fail;
   if((xzf = new(xzFile)) == NULL)
     goto fail;
-  if((xzf->m_fp = fopen(filename, mode)) != NULL) {
+  if((xzf->m_fp = fopen(filename, mode)) == NULL)
+    goto fail_free;
+
+  lzs = &xzf->m_lzma;
+  memcpy(lzs, &s_lzma_init, sizeof(*lzs));
+  if(lzma_stream_decoder(lzs, UINT64_MAX, LZMA_CONCATENATED) == LZMA_OK) {
+    lzs->next_in = xzf->m_inbuf;
+    lzs->avail_in = fread(xzf->m_inbuf, sizeof(uint8_t), IBSZ, xzf->m_fp);
     return xzf;
   }
 
+  fclose(xzf->m_fp);
+fail_free:
   free(xzf);
 fail:
   return (xzFile *) NULL;
@@ -57,9 +74,27 @@ fail:
 int
 xzread(xzFile *xzf, void *buf, int len)
 {
+  lzma_stream *lzs;
+
   if(xzf == NULL || xzf->m_fp == NULL || buf == NULL || len <= 0)
     return -1;
-  return fread(buf, sizeof(char), len, xzf->m_fp);
+
+  lzs = &xzf->m_lzma;
+  lzs->next_out = buf;
+  lzs->avail_out = len;
+  while(1) {
+    if(lzma_code(lzs, LZMA_RUN) != LZMA_OK) /* Argh! */
+      return -1;
+    if(! lzs->avail_out || feof(xzf->m_fp))
+      break;
+    /* lzma_code either exhausted the input buffer or filled the output
+     * buffer; we know it wasn't the latter.  We also know that we're
+     * not at EOF.  So: it's time to refill the input buffer. */
+    lzs->next_in = xzf->m_inbuf;
+    lzs->avail_in = fread(xzf->m_inbuf, sizeof(uint8_t), IBSZ, xzf->m_fp);
+  }
+
+  return lzs->next_out - (uint8_t *) buf;
 }
 
 void
@@ -71,6 +106,7 @@ xzclose(xzFile *xzf)
     fclose(xzf->m_fp);
     xzf->m_fp = NULL;
   }
+  lzma_end(&xzf->m_lzma);
   free(xzf);
 }
 
