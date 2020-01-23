2a5732af204d9926222d10472ff08b7d271310a4
Simon Burge <github@thistledown.com.au>
2020-01-24 10:14:32 +1100

    Fix out of bound array index.
    Check that next_state is defined (i.e. >= 0) before using as an array
    index into leads_to_result[].
    
    Fixes core dump when built with gcc7.

--- dfasyn/compdfa.c
+++ dfasyn/compdfa.c
@@ -425,7 +425,7 @@ do_next_dfa_state:
 
     for (j=0; j<ntokens; j++) {
       int next_state = dfas[i]->map[j];
-      if (leads_to_result[next_state] == 0) {
+      if (next_state >= 0 && leads_to_result[next_state] == 0) {
         dfas[i]->map[j] = -1;
       }
     }
